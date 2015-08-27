9c1213a3995c02e5e6f689ce13e17ae78068fd1b
Andrew Brennwald & Aileen Cacayorin <student@epicodus.com>
2015-08-27 16:00:01 -0700

    Add individual update buttons to recipe page

--- app.rb
+++ app.rb
@@ -38,17 +38,37 @@ get('/recipes/:id') do
   erb(:recipe)
 end
 
-patch('/recipes/:id') do
+
+patch("/recipes/:id/update_name") do
+
   id = params.fetch('id').to_i
+
   @recipe = Recipe.find(id)
-  name = params.fetch('recipe_name')
-  instructions = params.fetch('instructions')
-  ingredient_ids = params.fetch('ingredient_ids')
-  rating = params.fetch('rating').to_i
-  tag_ids = params.fetch('tag_ids')
-  @recipe.update({:name =>name, :instructions => instructions, :ingredient_ids => ingredient_ids, :rating => rating, :tag_ids => tag_ids})
+  name = params.fetch('name')
+  @recipe.update(:name => name)
   redirect back
 end
+
+patch("/recipes/:id/update_instructions") do
+
+  id = params.fetch('id').to_i
+
+  @recipe = Recipe.find(id)
+  instructions = params.fetch('instructions')
+  @recipe.update(:instructions => instructions)
+  redirect back
+end
+# patch('/recipes/:id') do
+#   id = params.fetch('id').to_i
+#   @recipe = Recipe.find(id)
+#   name = params.fetch('recipe_name')
+#   instructions = params.fetch('instructions')
+#   ingredient_ids = params.fetch('ingredient_ids')
+#   rating = params.fetch('rating').to_i
+#   tag_ids = params.fetch('tag_ids')
+#   @recipe.update({:name =>name, :instructions => instructions, :ingredient_ids => ingredient_ids, :rating => rating, :tag_ids => tag_ids})
+#   redirect back
+# end
 ### Ingredients ###
 
 get('/ingredients') do
@@ -60,6 +80,13 @@ post('/ingredients') do
   name = params.fetch("name")
   @ingredient = Ingredient.create({:name => name})
   redirect back
+end
+
+get('/ingredients/:id') do
+  id = params.fetch('id').to_i
+  @ingredient = Ingredient.find(id)
+  @recipes = @ingredient.recipes
+  erb(:ingredient)
 end
 
 
